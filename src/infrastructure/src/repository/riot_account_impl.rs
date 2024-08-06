410eed8822e757f8de9d482532b4d9814196f610
0x3fb <amakou1019@gmail.com>
2024-08-06 10:11:01 +0900

    chore: riot account impl change order

--- src/infrastructure/src/repository/riot_account_impl.rs
+++ src/infrastructure/src/repository/riot_account_impl.rs
@@ -24,7 +24,7 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
 
     let created_at_dt = chrono::DateTime::from_timestamp(account.created_at(), 0).unwrap().naive_utc();
     let dt = chrono::Utc::now().naive_utc();
-    
+
     let pear = riot_account::ActiveModel {
       id: Set(account.id().to_vec()),
       puuid: Set(account.puuid().to_string()),
@@ -34,9 +34,9 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
       created_at: Set(created_at_dt),
       updated_at: Set(dt),
     };
-    
+
     let result = riot_account::Entity::insert(pear).exec(&db).await;
-    
+
     match result {
       Ok(_) => Ok(account),
       Err(_) => Err(RiotAccountRepositoryError::InsertError)
@@ -67,6 +67,34 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
     }
   }
 
+  async fn find_by_game_name(&self, game_name: String) -> Result<RiotAccount, RiotAccountRepositoryError> {
+    let db = self.db.clone();
+    let result = riot_account::Entity::find()
+      .filter(riot_account::Column::GameName.contains(game_name))
+      .one(&db)
+      .await;
+
+    match result {
+      Ok(Some(pear)) => {
+        let id: ID = match pear.id.try_into() {
+          Ok(id) => id,
+          Err(_) => return Err(RiotAccountRepositoryError::ParseError)
+        };
+        let account = RiotAccount::new_riot_account_with_id(
+          id,
+          pear.puuid,
+          pear.game_name,
+          pear.tag_line,
+          pear.player_card,
+          pear.created_at.and_utc().timestamp(),
+          pear.updated_at.and_utc().timestamp(),
+        );
+        Ok(account)
+      },
+      Ok(None) => Err(RiotAccountRepositoryError::AccountNotFound),
+      Err(_) => Err(RiotAccountRepositoryError::QueryError)
+    }
+  }
   async fn find_by_puuid(&self, puuid: String) -> Result<RiotAccount, RiotAccountRepositoryError> {
     let db = self.db.clone();
     let result = riot_account::Entity::find()
@@ -95,41 +123,13 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
       Err(_) => Err(RiotAccountRepositoryError::QueryError)
     }
   }
-  async fn find_by_game_name(&self, game_name: String) -> Result<RiotAccount, RiotAccountRepositoryError> {
-    let db = self.db.clone();
-    let result = riot_account::Entity::find()
-      .filter(riot_account::Column::GameName.contains(game_name))
-      .one(&db)
-      .await;
-    
-    match result {
-      Ok(Some(pear)) => {
-        let id: ID = match pear.id.try_into() {
-          Ok(id) => id,
-          Err(_) => return Err(RiotAccountRepositoryError::ParseError)
-        };
-        let account = RiotAccount::new_riot_account_with_id(
-          id,
-          pear.puuid,
-          pear.game_name,
-          pear.tag_line,
-          pear.player_card,
-          pear.created_at.and_utc().timestamp(),
-          pear.updated_at.and_utc().timestamp(),
-        );
-        Ok(account)
-      },
-      Ok(None) => Err(RiotAccountRepositoryError::AccountNotFound),
-      Err(_) => Err(RiotAccountRepositoryError::QueryError)
-    }
-  }
-  
+
   async fn update(&self, id: ID, riot_account: RiotAccount) -> Result<RiotAccount, RiotAccountRepositoryError> {
     let db = self.db.clone();
 
     let created_at_dt = chrono::DateTime::from_timestamp(riot_account.created_at(), 0).unwrap().naive_utc();
     let dt = chrono::Utc::now().naive_utc();
-    
+
     let pear = riot_account::ActiveModel {
       id: Set(riot_account.id().to_vec()),
       puuid: Set(riot_account.puuid().to_string()),
@@ -139,9 +139,9 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
       created_at: Set(created_at_dt),
       updated_at: Set(dt),
     };
-    
+
     let result = pear.update(&db).await;
-    match result { 
+    match result {
       Ok(_) => Ok(riot_account),
       Err(_) => Err(RiotAccountRepositoryError::UpdateError)
     }
@@ -152,7 +152,7 @@ impl RiotAccountRepository for RiotAccountRepositoryImpl {
     let result = riot_account::Entity::delete_by_id(id.to_vec())
       .exec(&db)
       .await;
-    
+
     match result {
       Ok(_) => Ok(()),
       Err(_) => Err(RiotAccountRepositoryError::DeleteError)

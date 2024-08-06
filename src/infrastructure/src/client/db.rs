5bfa12bf106beae61bf9a9c4873ca110372dae05
0x3fb <amakou1019@gmail.com>
2024-08-06 10:07:51 +0900

    feat: db clients

---
+++ src/infrastructure/src/client/db.rs
@@ -0,0 +1,20 @@
+pub mod entity;
+
+use std::env;
+use std::time::Duration;
+use sea_orm::{ConnectOptions, Database, DbConn, DbErr};
+
+pub async fn db() -> Result<DbConn, DbErr> { 
+  let url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  let mut opt = ConnectOptions::new(&url);
+  opt
+    .max_connections(100)
+    .min_connections(5)
+    .connect_timeout(Duration::from_secs(8))
+    .acquire_timeout(Duration::from_secs(8))
+    .idle_timeout(Duration::from_secs(8))
+    .max_lifetime(Duration::from_secs(8))
+    .sqlx_logging(true);
+  
+  Database::connect(opt).await
+}

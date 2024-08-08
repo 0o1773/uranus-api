f5e0c0d95f001ea801c4c1cd2eb6ed55090f751d
0x3fb <amakou1019@gmail.com>
2024-08-08 13:06:43 +0900

    feat: agent domain

---
+++ src/domain/src/objects/agent.rs
@@ -0,0 +1,74 @@
+use ulid::Ulid;
+
+pub type ID = [u8; 16];
+
+pub struct Agent {
+  id: ID,
+  riot_agent_id: String,
+  name: String,
+  role: String,
+  icon: String,
+}
+
+impl Agent {
+  pub fn new_agent(riot_agent_id: String, name: String, role: String, icon: String) -> Agent {
+    let ulid = Ulid::new();
+    Agent {
+      id: ulid.to_bytes(),
+      riot_agent_id,
+      name,
+      role,
+      icon,
+    }
+  }
+  
+  pub fn new_agent_with_id(id: ID, riot_agent_id: String, name: String, role: String, icon: String) -> Agent {
+    Agent {
+      id,
+      riot_agent_id,
+      name,
+      role,
+      icon,
+    }
+  }
+  
+  pub fn riot_agent_id(&self) -> String {
+    self.riot_agent_id.clone()
+  }
+  
+  pub fn name(&self) -> String {
+    self.name.clone()
+  }
+  
+  pub fn role(&self) -> String {
+    self.role.clone()
+  }
+  
+  pub fn icon(&self) -> String {
+    self.icon.clone()
+  }
+  
+  pub fn id(&self) -> ID {
+    self.id.clone()
+  }
+  
+  pub fn set_riot_agent_id(&mut self, riot_agent_id: String) {
+    self.riot_agent_id = riot_agent_id;
+  }
+  
+  pub fn set_name(&mut self, name: String) {
+    self.name = name;
+  }
+  
+  pub fn set_role(&mut self, role: String) {
+    self.role = role;
+  }
+  
+  pub fn set_icon(&mut self, icon: String) {
+    self.icon = icon;
+  }
+  
+  pub fn set_id(&mut self, id: ID) {
+    self.id = id;
+  }
+}
---
+++ src/domain/src/traits/agent.rs
@@ -0,0 +1,12 @@
+use crate::objects::agent::{Agent, ID};
+use crate::objects::error::AgentRepositoryError;
+
+pub trait AgentRepository: Send + Sync + 'static {
+  async fn create(&self, riot_agent_id: String, name: String, role: String, icon: String) -> Result<Agent, AgentRepositoryError>;
+  async fn find_by_id(&self, id: ID) -> Result<Agent, AgentRepositoryError>;
+  async fn find_by_agent_id(&self, riot_agent_id: String) -> Result<Agent, AgentRepositoryError>;
+  async fn update(&self, id: ID, agent: Agent) -> Result<Agent, AgentRepositoryError>;
+  async fn delete(&self, id: ID) -> Result<(), AgentRepositoryError>;
+  async fn list(&self) -> Result<Vec<Agent>, AgentRepositoryError>;
+  async fn list_by_role(&self, role: String) -> Result<Vec<Agent>, AgentRepositoryError>;
+}

5c18fcc4909010fa6a8cd6f60421cddd11dc6d52
0x3fb <amakou1019@gmail.com>
2024-08-08 13:06:50 +0900

    feat: map domain

---
+++ src/domain/src/objects/map.rs
@@ -0,0 +1,64 @@
+use ulid::Ulid;
+
+pub type ID = [u8; 16];
+
+pub struct Map {
+  id: ID,
+  map_id: String,
+  name: String,
+  image: String,
+}
+
+impl Map {
+  pub fn new_map(map_id: String, name: String, image: String) -> Map {
+    let ulid = Ulid::new();
+    Map {
+      id: ulid.to_bytes(),
+      map_id,
+      name,
+      image,
+    }
+  }
+  
+  pub fn new_map_with_id(id: ID, map_id: String, name: String, image: String) -> Map {
+    Map {
+      id,
+      map_id,
+      name,
+      image,
+    }
+  }
+  
+  pub fn map_id(&self) -> String {
+    self.map_id.clone()
+  }
+  
+  pub fn name(&self) -> String {
+    self.name.clone()
+  }
+  
+  pub fn image(&self) -> String {
+    self.image.clone()
+  }
+  
+  pub fn id(&self) -> ID {
+    self.id.clone()
+  }
+  
+  pub fn set_map_id(&mut self, map_id: String) {
+    self.map_id = map_id;
+  }
+  
+  pub fn set_name(&mut self, name: String) {
+    self.name = name;
+  }
+  
+  pub fn set_image(&mut self, image: String) {
+    self.image = image;
+  }
+  
+  pub fn set_id(&mut self, id: ID) {
+    self.id = id;
+  }
+}
+
---
+++ src/domain/src/traits/map.rs
@@ -0,0 +1,10 @@
+use crate::objects::error::MapRepositoryError;
+use crate::objects::map::{Map, ID};
+
+pub trait MapRepository: Sync + Send + 'static { 
+  async fn create(&self, name: String, image: String) -> Result<Map, MapRepositoryError>;
+  async fn find_by_id(&self, id: ID) -> Result<Map, MapRepositoryError>;
+  async fn update(&self, id: ID, map: Map) -> Result<Map, MapRepositoryError>;
+  async fn delete(&self, id: ID) -> Result<(), MapRepositoryError>;
+  async fn list(&self) -> Result<Vec<Map>, MapRepositoryError>;
+}
